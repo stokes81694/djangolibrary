c58cb352769ff1acabe6e68dafdac5351ab40da4
stokes81694 <58713668+stokes81694@users.noreply.github.com>
2020-04-06 11:25:40 -0700

    Update views.py
    Made problematic view a class with a property

--- catalog/views.py
+++ catalog/views.py
@@ -4,34 +4,35 @@ from django.shortcuts import render
 
 from catalog.models import Book, Author, BookInstance, Genre
 
+class indexView(request):
+    @property
+    def index(self):
+        #"""View function for home page of site."""
 
-def index(request):
-    """View function for home page of site."""
+        # Generate counts of some of the main objects
+        num_books = Book.objects.all().count()
+        num_instances = BookInstance.objects.all().count()
 
-    # Generate counts of some of the main objects
-    # num_books = Book.objects.all().count()
-    num_instances = BookInstance.objects.all().count()
+        # Available books (status = 'a')
+        num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
-    # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+        # The 'all()' is implied by default.
+        num_authors = Author.objects.count()
 
-    # The 'all()' is implied by default.
-    num_authors = Author.objects.count()
+        # Number of visits to this view, as counted in the session variable.
+        num_visits = request.session.get('num_visits', 0)
+        request.session['num_visits'] = num_visits + 1
 
-    # Number of visits to this view, as counted in the session variable.
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+        context = {
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_visits': num_visits,
+        }
 
-    context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-        'num_visits': num_visits,
-    }
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+        # Render the HTML template index.html with the data in the context variable
+        return render(request, 'index.html', context=context)
 
 from django.views import generic
 
